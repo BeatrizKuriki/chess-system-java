8bc80cc90ee7600f7c99d2bf3252e765f6b0e759
BeatrizKuriki <b.f.kuriki@gmail.com>
2022-07-29 09:03:46 -0300

    Ui, King moves, Rook moves etc

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -1,10 +1,13 @@
 package chess;
+
 import boardgame.Board;
 import boardgame.Piece;
+import boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
-	
+
 	private Color color;
+	private int moveCount;
 
 	public ChessPiece(Board board, Color color) {
 		super(board);
@@ -14,8 +17,25 @@ public abstract class ChessPiece extends Piece {
 	public Color getColor() {
 		return color;
 	}
-
-
+	
+	public int getMoveCount() {
+		return moveCount;
 	}
 	
+	protected void increaseMoveCount() {
+		moveCount++;
+	}
+
+	protected void decreaseMoveCount() {
+		moveCount--;
+	}
+
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
+	}
 	
+	protected boolean isThereOpponentPiece(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p != null && p.getColor() != color;
+	}
+}
